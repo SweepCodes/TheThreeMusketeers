9f5826c84408998e4d951131cdb0cff762c7c503
FilipFE23 <filip.bockarovski@gritacademy.se>
2024-03-19 14:07:20 +0100

    Temp eventlisteners

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ const logInRegisterPage = document.getElementById("log-in-register-page") as HTM
 logInRegisterPage.addEventListener("click", (event) => {
   if(event.target instanceof HTMLElement && event.target.classList.contains("toggle-pages")){
     const logInDiv = document.getElementById("log-in-div") as HTMLDivElement;
-    const logInForm = document.getElementById("log-in-form") as HTMLFormElement;
     const registerDiv = document.getElementById("register-div") as HTMLDivElement;
+    const logInForm = document.getElementById("log-in-form") as HTMLFormElement;
     const registerForm = document.getElementById("register-form") as HTMLFormElement;
 
     logInForm.reset();
@@ -14,3 +14,31 @@ logInRegisterPage.addEventListener("click", (event) => {
     registerDiv.classList.toggle("hidden");
   };
 });
+
+///// Temporary /////
+const logInForm = document.getElementById("log-in-form") as HTMLFormElement;
+const registerForm = document.getElementById("register-form") as HTMLFormElement;
+
+logInForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const logInDiv = document.getElementById("log-in-div") as HTMLDivElement;
+  const navBar = document.getElementById("nav-bar") as HTMLDivElement;
+  const homePageDiv = document.getElementById("home-page") as HTMLDivElement;
+  
+  logInDiv.classList.toggle("hidden");
+  navBar.classList.remove("hidden");
+  homePageDiv.classList.remove("hidden");
+});
+
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const registerDiv = document.getElementById("register-div") as HTMLDivElement;
+  const navBar = document.getElementById("nav-bar") as HTMLDivElement;
+  const homePageDiv = document.getElementById("home-page") as HTMLDivElement;
+  
+  registerDiv.classList.toggle("hidden");
+  navBar.classList.remove("hidden");
+  homePageDiv.classList.remove("hidden");
+
+})
+/////////////////////
